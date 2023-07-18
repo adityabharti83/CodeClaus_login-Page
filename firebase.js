27ef1c02139a3af4505952761df5066b878ad514
Aditya Bharti <135196678+adityabharti83@users.noreply.github.com>
2023-07-18 20:56:06 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,9 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyDr9k1hAFFvl-981LXKOUKgrV1g6_8EysQ",
+  authDomain: "computer-information-c4045.firebaseapp.com",
+  databaseURL: "https://computer-information-c4045.firebaseio.com",
+  projectId: "computer-information-c4045"
+    // ... (other credentials)
+  };
+  firebase.initializeApp(firebaseConfig);
---
+++ main.js
@@ -0,0 +1,72 @@
+ function signInWithGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(function(result) {
+        // Handle successful login
+        var user = result.user;
+        console.log(user);
+        redirectToPage('https://www.linkedin.com/in/aditya-bharti-578b90231/');
+      })
+      .catch(function(error) {
+        // Handle error
+        console.log(error);
+      });
+  }
+
+  function signInWithFacebook() {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(function(result) {
+        // Handle successful login
+        var user = result.user;
+        console.log(user);
+        redirectToPage('https://www.linkedin.com/in/aditya-bharti-578b90231/');
+      })
+      .catch(function(error) {
+        // Handle error
+        console.log(error);
+      });
+  }
+
+  function signInWithGitHub() {
+    var provider = new firebase.auth.GithubAuthProvider();
+    firebase.auth().signInWithPopup(provider)
+      .then(function(result) {
+        // Handle successful login
+        var user = result.user;
+        console.log(user);
+        redirectToPage('https://www.linkedin.com/in/aditya-bharti-578b90231/');
+      })
+      .catch(function(error) {
+        // Handle error
+        console.log(error);
+      });
+  }
+
+  function signInWithOTP() {
+    var num = document.getElementById('ph-num').value;
+    var phoneNumber = "+91" + num; // Replace with user's phone number
+    console.log(phoneNumber);
+    var appVerifier = new firebase.auth.RecaptchaVerifier('recaptcha-container');
+    firebase.auth().signInWithPhoneNumber(phoneNumber, appVerifier)
+      .then(function(confirmationResult) {
+        // OTP sent successfully
+        var verificationCode = prompt("Please enter the verification code sent to your phone.");
+        return confirmationResult.confirm(verificationCode);
+      })
+      .then(function(result) {
+        // Handle successful OTP login
+        var user = result.user;
+        console.log(user);
+        redirectToPage('https://www.linkedin.com/in/aditya-bharti-578b90231/');
+      })
+      .catch(function(error) {
+        // Handle error
+        console.log(error);
+      });
+  }
+
+
+  // re function
+
+  redirectToPage('https://www.linkedin.com/in/aditya-bharti-578b90231/');
